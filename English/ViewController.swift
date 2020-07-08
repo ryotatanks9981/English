a62ab990fb528cce512dfdc920ec82ddff0ed7cf
ryotatanks9981 <67944448+ryotatanks9981@users.noreply.github.com>
2020-07-08 12:30:48 +0900

    Update ViewController.swift

--- English/ViewController.swift
+++ English/ViewController.swift
@@ -1,11 +1,3 @@
-//
-//  ViewController.swift
-//  English
-//
-//  Created by Ryota Karita on 2020/05/28.
-//  Copyright © 2020 Ryota Karita. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
